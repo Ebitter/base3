5d9a0886ade6ca61f76a51c65ab2aebd2e53750d
ebitter <991807771@qq.com>
2018-05-28 16:23:12 +0800

    base3

---
+++ main.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+#include <thread>
+#include<iostream>
+#include <mutex>
+std::mutex mutex;
+int source = 1;
+void task(const char *agr)
+{
+    for(;;)
+    {
+        mutex.lock();
+        if(source>100)
+        {
+            mutex.unlock();
+            break;
+        }
+       std::cout  << agr << "  i=" << source++  << " thread ID=" << std::this_thread::get_id() << std::endl;
+       mutex.unlock();
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+
+    printf("hello,world!\n");
+
+    std::thread threadOne(task, "taskOne");
+    std::thread threadTwo(task, "taskTwo");
+
+    threadOne.join();
+    threadTwo.join();
+}
